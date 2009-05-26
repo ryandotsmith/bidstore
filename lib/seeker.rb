f87784033dc75acd0d79d46f29b96a62e13d6479
Ryan Smith <this.ryansmith@gmail.com>
2009-05-26 14:36:45 -0500

    pulled the local vars out of the seeker.filter method and added them as instance variables REFACTOR

--- lib/seeker.rb
+++ lib/seeker.rb
@@ -8,40 +8,38 @@ end
 class Seeker
 
   def initialize( string )
-    @q_string = string
+    @q_string     =   string
+    @origin       =   String.new
+    @destination  =   String.new
+    @includes     =   Array.new
   end
 
   def filter_query()                                         	
     # if the query specifies an object, pick it from string
-    objects = String.new
-    origin  = ""
-    destination = ""
     if !@q_string.scan(/:/).length.zero? # string has a :
-      objects   = @q_string.split(':').first 
+      @includes << @q_string.split(':').first
       remainder = @q_string.split(':').second
       if !remainder.scan(/-/).length.zero?
-        origin      = remainder.split('-').first  unless remainder.split('-').first.length.zero?
-        destination = remainder.split('-').last unless remainder.split('-').last.length.zero? 
+        @origin      = remainder.split('-').first  unless remainder.split('-').first.length.zero?
+        @destination = remainder.split('-').last unless remainder.split('-').last.length.zero? 
       else
-        origin      =	remainder
-        destination = remainder
+        @origin      =	remainder
+        @destination =  remainder
       end 
     elsif !@q_string.scan(/-/).length.zero? # string has -
       if @q_string.first == "-"
-        origin = ""
-        destination = @q_string.split('-').last
+        @destination = @q_string.split('-').last
       elsif @q_string.last == "-" 
-        origin = @q_string.split('-').first
-        destination = ""
+        @origin = @q_string.split('-').first
       else
-        origin      = @q_string.split('-').first
-        destination = @q_string.split('-').last
+        @origin      = @q_string.split('-').first
+        @destination = @q_string.split('-').last
       end
     else
-      origin      = @q_string
-      destination = @q_string
+      @origin      = @q_string
+      @destination = @q_string
     end
-    { :objects => objects.peel, :origin => origin.peel, :destination => destination.peel }
+    { :objects => @includes.first, :origin => @origin.peel, :destination => @destination.peel }
   end
 
 end

700892e260b5d65f4489c1c1f609f0f4827378f3
Ryan Smith <this.ryansmith@gmail.com>
2009-06-02 09:30:01 -0500

    added a method to make dates pretty and added some padding between the edit links in the context nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,11 @@ module ApplicationHelper
   def n_a_ify( string )
     string.length.zero? ? %W( NA ) : string
   end
-
+  
+  def date_ify( date )
+    date.strftime('%m/%d/%Y')
+  end
+  
   def get_css( controller )
     [controller.controller_name, 'application','tablesort/tablesort.css','humanity/syle.css']
   end
